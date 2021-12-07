91de2c60bc4486b15de88988b9f42cc3633dbae1
sujithmannarkkad <74753686+sujithmannarkkad@users.noreply.github.com>
2021-12-07 19:44:13 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+print('Environment is ',app.config["ENV"])
 if app.config["ENV"] == "production":
     app.config.from_object("config.ProductionConfig")
 elif app.config["ENV"] == "testing":
